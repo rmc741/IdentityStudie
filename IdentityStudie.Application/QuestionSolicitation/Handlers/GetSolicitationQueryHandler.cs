330ccae31b7777abacf7a1fbc5f348ccd361db24
RafaelMendonca <rafael.medonca2@gmail.com>
2024-04-22 20:15:17 -0300

    Cria os dois handler para Get dados.

---
+++ IdentityStudie.Application/QuestionSolicitation/Handlers/GetSolicitationQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using IdentityStudie.Application.QuestionSolicitation.Queries;
+using IdentityStudie.Domain.Entities;
+using MediatR;
+
+namespace IdentityStudie.Application.QuestionSolicitation.Handlers
+{
+    public class GetSolicitationQueryHandler : IRequestHandler<GetSolicitationsQuery, IEnumerable<Domain.Entities.QuestionSolicitation>>
+    {
+        //private readonly ISolicitationRepository _solicitationRepository;
+
+        public GetSolicitationQueryHandler()
+        {
+        }
+
+        public Task<IEnumerable<Domain.Entities.QuestionSolicitation>> Handle(GetSolicitationsQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
