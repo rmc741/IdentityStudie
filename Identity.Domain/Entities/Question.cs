d0f664ef9176445fe7272a66e9f7b26ee97db606
RafaelMendonca <rafael.medonca2@gmail.com>
2024-04-22 11:43:00 -0300

    Adicionado validacao para Solicitacoes de questoes.

--- Identity.Domain/Entities/Question.cs
+++ Identity.Domain/Entities/Question.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Identity.Domain.Entities
+﻿namespace Identity.Domain.Entities
 {
     public class Question : BaseEntity
     {
--- Identity.Domain/Entities/QuestionSolicitation.cs
+++ Identity.Domain/Entities/QuestionSolicitation.cs
@@ -12,12 +12,7 @@ namespace Identity.Domain.Entities
 
         public QuestionSolicitation(string description, int totalQuestions, int categoryId)
         {
-            DomainExceptionValidation.When(string.IsNullOrEmpty(description), "Invalid description. Description is required.");
-            DomainExceptionValidation.When(totalQuestions <= 0, "Invalid Total Questions Request value.");
-            DomainExceptionValidation.When(categoryId <= 0, "Invalid Category Id value.");
-            Description = description;
-            TotalQuestionsRequest = totalQuestions;
-            CategoryId = categoryId;
+            ValidateDomain(description, totalQuestions, categoryId);
         }
 
         public QuestionSolicitation(int id, string description, int totalQuestions, int categoryId)
@@ -25,6 +20,16 @@ namespace Identity.Domain.Entities
             DomainExceptionValidation.When(id < 0, "Invalid id value.");
             Id = id;
 
+            ValidateDomain(description,totalQuestions,categoryId);
+        }
+
+        public void Update(string description, int totalQuestions, int categoryId)
+        {
+            ValidateDomain(description, totalQuestions, categoryId);
+        }
+
+        private void ValidateDomain(string description, int totalQuestions, int categoryId)
+        {
             DomainExceptionValidation.When(string.IsNullOrEmpty(description), "Invalid description. Description is required.");
             DomainExceptionValidation.When(totalQuestions <= 0, "Invalid Total Questions Request value.");
             DomainExceptionValidation.When(categoryId <= 0, "Invalid Category Id value.");
@@ -36,56 +41,7 @@ namespace Identity.Domain.Entities
         /*
         //public string UserId { get; set; }
         //public ApplicationUser User { get; set; }
-        //public int? TotalQuestions { get => Questions?.Count; }*/
-
-        /*
-        //public Product(string name, string description, decimal price, int stock, string image)
-        //{
-        //    ValidateDomain(name, description, price, stock, image);
-        //}
-        //
-        //public Product(int id, string name, string description, decimal price, int stock, string image)
-        //{
-        //    DomainExceptionValidation.When(id < 0, "Invalid Id value.");
-        //    Id = id;
-        //    ValidateDomain(name, description, price, stock, image);
-        //}
-        //
-        //public void Update(string name, string description, decimal price, int stock, string image, int categoryId)
-        //{
-        //    ValidateDomain(name, description, price, stock, image);
-        //    CategoryId = categoryId;
-        //}
-        //
-        //private void ValidateDomain(string name, string description, decimal price, int stock, string image)
-        //{
-        //    DomainExceptionValidation.When(string.IsNullOrEmpty(name),
-        //        "Invalid name. Name is required");
-        //
-        //    DomainExceptionValidation.When(name.Length < 3,
-        //        "Invalid name, too short, minimum 3 characters");
-        //
-        //    DomainExceptionValidation.When(string.IsNullOrEmpty(description),
-        //        "Invalid description. Description is required");
-        //
-        //    DomainExceptionValidation.When(description.Length < 5,
-        //        "Invalid description, too short, minimum 5 characters");
-        //
-        //    DomainExceptionValidation.When(price < 0, "Invalid price value");
-        //
-        //    DomainExceptionValidation.When(stock < 0, "Invalid stock value");
-        //
-        //    DomainExceptionValidation.When(image?.Length > 250,
-        //        "Invalid image name, too long, maximum 250 characters");
-        //
-        //    Name = name;
-        //    Description = description;
-        //    Price = price;
-        //    Stock = stock;
-        //    Image = image;
-        //}
-        //public int CategoryId { get; set; }
-        //public Category Category { get; set; }
+        //public int? TotalQuestions { get => Questions?.Count; }
         */
     }
 }
