05814caa04f5f9f365494f549be5c91d2910fe2c
RafaelMendonca <rafael.medonca2@gmail.com>
2024-04-22 18:01:05 -0300

    adiciona interface para solicitation service e mapeamento dos DTOs

--- IdentityStudie.Application/Interfaces/IQuestionSolicitationService.cs
+++ IdentityStudie.Application/Interfaces/IQuestionSolicitationService.cs
@@ -4,9 +4,9 @@ namespace IdentityStudie.Application.Interfaces
 {
     public interface IQuestionSolicitationService
     {
-        Task<IEnumerable<QuestionSolicitationDTO>> GetProducts();
+        Task<IEnumerable<QuestionSolicitationDTO>> GetSolicitations();
         Task Add(QuestionSolicitationDTO questionSolicictationDto);
-        Task<QuestionSolicitationDTO> GetById(int? id);
+        Task<QuestionSolicitationDTO> GetById(int id);
         Task Update(QuestionSolicitationDTO questionSolicictationDto);
         Task Remove(int id);
     }
---
+++ IdentityStudie.Application/Mappings/DomainToDtoMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Identity.Domain.Entities;
+using IdentityStudie.Application.DTOs;
+
+namespace IdentityStudie.Application.Mappings
+{
+    public class DomainToDtoMappingProfile : Profile
+    {
+        public DomainToDtoMappingProfile()
+        {
+            CreateMap<QuestionSolicitation, QuestionSolicitationDTO>.ReverseMap();
+        }
+    }
+}
