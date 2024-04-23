b166acf5877168e007cb4df6e8eca168433ee383
RafaelMendonca <rafael.medonca2@gmail.com>
2024-04-23 17:09:30 -0300

    Adiciona entidades para db context, adiciona configução para solicitacoes e finalizado implementacao de repository para solicitacoes.

---
+++ IdentityStudie.Infra/Data/Repository/SolicitationRepository.cs
@@ -0,0 +1,49 @@
+﻿using IdentityStudie.Domain.Entities;
+using IdentityStudie.Domain.Interfaces;
+using IdentityStudie.Infra.Data.Context;
+using System.Data.Entity;
+
+namespace IdentityStudie.Infra.Data.Repository
+{
+    public class SolicitationRepository : ISolicitationRepository
+    {
+        private ApplicationDbContext _dbContext;
+
+        public SolicitationRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<QuestionSolicitation> CreateAsync(QuestionSolicitation solicitation)
+        {
+            _dbContext.Add(solicitation);
+            await _dbContext.SaveChangesAsync();
+            return solicitation;
+        }
+
+        public async Task<QuestionSolicitation> GetByIdAsync(int id)
+        {
+            return await _dbContext.Solicitations.Include(c => c.Category)
+                .SingleOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task<IEnumerable<QuestionSolicitation>> GetSolicitationsAsync()
+        {
+            return await _dbContext.Solicitations.ToListAsync();
+        }
+
+        public async Task<QuestionSolicitation> RemoveAsync(QuestionSolicitation solicitation)
+        {
+            _dbContext.Remove(solicitation);
+            await _dbContext.SaveChangesAsync();
+            return solicitation;
+        }
+
+        public async Task<QuestionSolicitation> UpdateAsync(QuestionSolicitation solicitation)
+        {
+            _dbContext.Update(solicitation);
+            await _dbContext.SaveChangesAsync();
+            return solicitation;
+        }
+    }
+}
