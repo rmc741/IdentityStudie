c5a72c64d40ca6c1a306eb9ce65eff4c37a94e1f
RafaelMendonca <rafael.medonca2@gmail.com>
2024-04-29 16:16:35 -0300

    adiciona json ignore para category

--- IdentityStudie.Domain/Entities/Category.cs
+++ IdentityStudie.Domain/Entities/Category.cs
@@ -1,8 +1,12 @@
-﻿namespace IdentityStudie.Domain.Entities
+﻿using System.Text.Json.Serialization;
+
+namespace IdentityStudie.Domain.Entities
 {
     public class Category : BaseEntity
     {
         public string Name { get; private set; }
+
+        [JsonIgnore]
         public ICollection<QuestionSolicitation> QuestionsSolicitations { get; set; }
     }
 }
---
+++ IdentityStudie.Domain/Enums/CategoryEnum.cs
@@ -0,0 +1,10 @@
+﻿namespace IdentityStudie.Domain.Enums
+{
+    public enum CategoryEnum
+    {
+        Fisica = 2,
+        Matematica = 5,
+        Portugues = 6,
+        Quimica = 8,
+    }
+}
