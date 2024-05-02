9dcbe153dbae9481b6629f91d190b883f5e026f9
RafaelMendonca <rafael.medonca2@gmail.com>
2024-05-02 12:31:30 -0300

    adiciona status e professor id como novas propriedades.

--- IdentityStudie.Domain/Entities/QuestionSolicitation.cs
+++ IdentityStudie.Domain/Entities/QuestionSolicitation.cs
@@ -1,4 +1,5 @@
-﻿using IdentityStudie.Domain.Validation;
+﻿using IdentityStudie.Domain.Enums;
+using IdentityStudie.Domain.Validation;
 
 namespace IdentityStudie.Domain.Entities;
 
@@ -7,6 +8,8 @@ public sealed class QuestionSolicitation : BaseEntity
     public string Description { get; set; }
     public int TotalQuestionsRequest { get;  set; }
     public int CategoryId { get; set; }
+    public int ProfessorId { get; set; }
+    public StatusEnum Status { get; set; } = StatusEnum.Active;
     public Category Category { get; set; }
     public ICollection<Question>? QuestionList { get; set; }
 
@@ -15,32 +18,34 @@ public sealed class QuestionSolicitation : BaseEntity
         
     }
 
-    public QuestionSolicitation(string description, int totalQuestions, int categoryId)
+    public QuestionSolicitation(string description, int totalQuestions, int categoryId, StatusEnum status)
     {
-        ValidateDomain(description, totalQuestions, categoryId);
+        ValidateDomain(description, totalQuestions, categoryId, status);
     }
 
-    public QuestionSolicitation(int id, string description, int totalQuestions, int categoryId)
+    public QuestionSolicitation(int id, string description, int totalQuestions, int categoryId, StatusEnum status)
     {
         DomainExceptionValidation.When(id < 0, "Invalid id value.");
         Id = id;
 
-        ValidateDomain(description,totalQuestions,categoryId);
+        ValidateDomain(description,totalQuestions,categoryId, status);
     }
 
-    public void Update(string description, int totalQuestions, int categoryId)
+    public void Update(string description, int totalQuestions, int categoryId, StatusEnum status)
     {
-        ValidateDomain(description, totalQuestions, categoryId);
+        ValidateDomain(description, totalQuestions, categoryId, status);
     }
 
-    private void ValidateDomain(string description, int totalQuestions, int categoryId)
+    private void ValidateDomain(string description, int totalQuestions, int categoryId, StatusEnum status)
     {
         DomainExceptionValidation.When(string.IsNullOrEmpty(description), "Invalid description. Description is required.");
         DomainExceptionValidation.When(totalQuestions <= 0, "Invalid Total Questions Request value.");
         DomainExceptionValidation.When(categoryId <= 0, "Invalid Category Id value.");
+        DomainExceptionValidation.When(Enum.IsDefined(status), "Invalid Status value.");
         Description = description;
         TotalQuestionsRequest = totalQuestions;
         CategoryId = categoryId;
+        Status = status;
     }
 
     /*
