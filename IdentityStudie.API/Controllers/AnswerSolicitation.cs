81a5de29a78ebd1d2f7dc43bbee50a73a8c64338
RafaelMendonca <rafael.medonca2@gmail.com>
2024-05-02 11:23:11 -0300

    endpoint para retorno de solicitacoes por categoria.

--- IdentityStudie.API/Controllers/AnswerSolicitation.cs
+++ IdentityStudie.API/Controllers/AnswerSolicitation.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using IdentityStudie.Application.DTOs;
+using IdentityStudie.Application.Interfaces;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IdentityStudie.API.Controllers
@@ -7,5 +9,29 @@ namespace IdentityStudie.API.Controllers
     [ApiController]
     public class AnswerSolicitation : ControllerBase
     {
+        private readonly IAnswerSolicitationService _answerSolicitationService;
+
+        public AnswerSolicitation(IAnswerSolicitationService answerSolicitations)
+        {
+            _answerSolicitationService = answerSolicitations;
+        }
+
+        [HttpGet("{categoryId}")]
+        public async Task<ActionResult<IEnumerable<QuestionSolicitationDTO>>> GetSolicitationsByCategoryId(int categoryId)
+        {
+            var solicitationsList = await _answerSolicitationService.GetSolicitationsByCategoryId(categoryId);
+            if (solicitationsList == null)
+            {
+                return NotFound("Solicitations not found");
+            }
+
+            return Ok(solicitationsList);
+        }
+
+        /*
+         * TO-DO 
+         * ENDPOINTS para Update e get solicitações por id
+         * 
+         */
     }
 }
