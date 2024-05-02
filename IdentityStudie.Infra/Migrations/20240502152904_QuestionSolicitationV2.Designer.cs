34bca2b46692b8a27d7f845e98d73b77f73b1242
RafaelMendonca <rafael.medonca2@gmail.com>
2024-05-02 12:31:51 -0300

    migrations

--- IdentityStudie.Infra/Migrations/20240502152904_QuestionSolicitationV2.Designer.cs
+++ IdentityStudie.Infra/Migrations/20240502152904_QuestionSolicitationV2.Designer.cs
@@ -4,6 +4,7 @@ using IdentityStudie.Infra.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace IdentityStudie.Infra.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240502152904_QuestionSolicitationV2")]
+    partial class QuestionSolicitationV2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ IdentityStudie.Infra/Migrations/20240502152904_QuestionSolicitationV2.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IdentityStudie.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class QuestionSolicitationV2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ProfessorId",
+                table: "Solicitations",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Solicitations",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfessorId",
+                table: "Solicitations");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Solicitations");
+        }
+    }
+}
