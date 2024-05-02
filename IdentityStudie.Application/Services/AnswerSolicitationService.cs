d5f307c27b101cc6674747c6f88387933a538024
RafaelMendonca <rafael.medonca2@gmail.com>
2024-05-02 11:24:46 -0300

    adiciona service para responder solicitacoes.

---
+++ IdentityStudie.Application/Services/AnswerSolicitationService.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using IdentityStudie.Application.AnswerSolicitation.Queries;
+using IdentityStudie.Application.DTOs;
+using IdentityStudie.Application.Interfaces;
+using MediatR;
+
+namespace IdentityStudie.Application.Services
+{
+    public class AnswerSolicitationService : IAnswerSolicitationService
+    {
+        private readonly IMapper _mapper;
+        private readonly IMediator _mediator;
+
+        public AnswerSolicitationService(IMapper mapper, IMediator mediator)
+        {
+            _mapper = mapper;
+            _mediator = mediator;
+        }
+
+        public async Task<IEnumerable<QuestionSolicitationDTO>> GetSolicitationsByCategoryId(int categoryId)
+        {
+            var solicitationsByCategoryIdQuery = new GetSolicitationByCategoryIdQuery(categoryId);
+
+            if (solicitationsByCategoryIdQuery == null)
+                throw new Exception($"Entity could not be loaded.");
+
+            var result = await _mediator.Send(solicitationsByCategoryIdQuery);
+
+            return _mapper.Map<IEnumerable<QuestionSolicitationDTO>>(result);
+        }
+    }
+}
