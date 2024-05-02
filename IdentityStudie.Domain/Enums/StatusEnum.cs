9367afc868174f80cf799fa29eee7be116a02172
RafaelMendonca <rafael.medonca2@gmail.com>
2024-05-02 13:05:08 -0300

    retira code smells.

--- IdentityStudie.Domain/Enums/StatusEnum.cs
+++ IdentityStudie.Domain/Enums/StatusEnum.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace IdentityStudie.Domain.Enums
+﻿namespace IdentityStudie.Domain.Enums
 {
     public enum StatusEnum
     {
         Active,
         Inactive,
         Finished,
-        Closed        
+        Closed
     }
 }
