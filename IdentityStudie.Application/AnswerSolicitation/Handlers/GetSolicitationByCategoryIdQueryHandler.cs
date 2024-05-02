7f73388134ee72c2503ff175ae8a666ae4ab24f4
RafaelMendonca <rafael.medonca2@gmail.com>
2024-05-02 11:23:51 -0300

    adiciona handler para busca de solicitacoes por categoria id

---
+++ IdentityStudie.Application/AnswerSolicitation/Handlers/GetSolicitationByCategoryIdQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using IdentityStudie.Application.AnswerSolicitation.Queries;
+using IdentityStudie.Domain.Interfaces;
+using MediatR;
+
+namespace IdentityStudie.Application.AnswerSolicitation.Handlers
+{
+    public class GetSolicitationByCategoryIdQueryHandler : IRequestHandler<GetSolicitationByCategoryIdQuery, IEnumerable<Domain.Entities.QuestionSolicitation>>
+    {
+        private readonly IAnswerSolicitationRepository _answerSolicitationRepository;
+
+        public GetSolicitationByCategoryIdQueryHandler(IAnswerSolicitationRepository answerSolicitationRepository)
+        {
+            _answerSolicitationRepository = answerSolicitationRepository;
+        }
+
+        public async Task<IEnumerable<Domain.Entities.QuestionSolicitation>> Handle(GetSolicitationByCategoryIdQuery request, CancellationToken cancellationToken)
+        {
+            /*
+             * TO-DO
+             * Implementar busca da lista de solicitações por category id
+             */
+
+            try
+            {
+                return await _answerSolicitationRepository.GetSolicitationsByCategoryIdAsync(request.Id);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }            
+        }
+    }
+}
