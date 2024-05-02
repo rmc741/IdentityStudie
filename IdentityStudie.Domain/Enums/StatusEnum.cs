0aaf0874a3b70b2c8c4a2ae10e473e4228fc9f1d
RafaelMendonca <rafael.medonca2@gmail.com>
2024-05-02 12:31:43 -0300

    cria novo enum para status.

---
+++ IdentityStudie.Domain/Enums/StatusEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityStudie.Domain.Enums
+{
+    public enum StatusEnum
+    {
+        Active,
+        Inactive,
+        Finished,
+        Closed        
+    }
+}
