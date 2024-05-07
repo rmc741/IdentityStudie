daac5c043c454e136ab371240800f26fd56407ce
RafaelMendonca <rafael.medonca2@gmail.com>
2024-05-07 13:40:06 -0300

    Adiciona handler e requests para answer solicitation

---
+++ IdentityStudie.Application/AnswerSolicitation/Queries/GetSolicitationByProfessorIdQuery.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace IdentityStudie.Application.AnswerSolicitation.Queries
+{
+    public class GetSolicitationByProfessorIdQuery : IRequest<IEnumerable<Domain.Entities.QuestionSolicitation>>
+    {
+        public int ProfessorId { get; set; }
+        public GetSolicitationByProfessorIdQuery(int id)
+        {
+            ProfessorId = id;
+        }
+    }
+}
