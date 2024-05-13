64a28a64d72c4da0599178822cf34e62a38436c0
RafaelMendonca <rafael.medonca2@gmail.com>
2024-05-13 13:19:06 -0300

    remove code smells.

--- IdentityStudie.API/Program.cs
+++ IdentityStudie.API/Program.cs
@@ -29,7 +29,6 @@ namespace IdentityStudie
 
             app.UseAuthorization();
 
-
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
