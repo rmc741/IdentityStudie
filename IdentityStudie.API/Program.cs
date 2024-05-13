ddb6b7f4ec37534b542b680f30925bbcd8eee585
RafaelMendonca <rafael.medonca2@gmail.com>
2024-05-13 14:14:15 -0300

    adiciona o uso de cors.

--- IdentityStudie.API/Program.cs
+++ IdentityStudie.API/Program.cs
@@ -18,6 +18,8 @@ namespace IdentityStudie
 
             var app = builder.Build();
 
+            app.UseCors();
+
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
